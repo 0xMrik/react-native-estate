8781dc450d2feb1ff39a8951c7fbc21833769986
0xMrik <agu.aymeric@hotmail.fr>
2025-05-19 18:15:28 +0200

    add assets

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { Link } from "expo-router";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -10,10 +10,11 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text className="text-4xl font-bold">Welcome to ReState</Text>
       <Link href="/sign-in">SignIn</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
-      <Link href="/properties/1">Property</Link>
+      <Link href={{ pathname: "/properties/[id]", params: { id: "1" } }}>Property</Link>
     </View>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Mettez à jour ceci pour inclure les chemins vers tous vos fichiers de composants.
+  content: [
+    "./App.tsx", 
+    "./components/**/*.{js,jsx,ts,tsx}", 
+    "./app/**/*.{js,jsx,ts,tsx}", 
+    "./components/**/*.{js,jsx,ts,tsx}"
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      fontFamily: {
+        rubik: ["Rubik-Regular", "sans-serif"],
+        rubikBold: ["Rubik-Bold", "sans-serif"],
+        rubikMedium: ["Rubik-Medium", "sans-serif"],
+        rubikSemiBold: ["Rubik-SemiBold", "sans-serif"],
+        rubikExtraBold: ["Rubik-ExtraBold", "sans-serif"],
+        rubikLight: ["Rubik-Light", "sans-serif"],
+      },
+      colors: {
+        primary: {
+          100: '#0061FF0A',
+          200: '#0061FF1A',
+          300: '#0061FF2A',
+        },
+        accent: {
+          100: '#FBFBFD',
+        },
+        black: {
+          DEFAULT: '#000000',
+          100: '#8C8E98',
+          200: '#666876',
+          300: '#191d31',
+        },
+        danger: "F75555"
+      },
+    },
+  },
+  plugins: [],
+}
