86ecbfd7de5331995e1623437616a9842426f501
0xMrik <agu.aymeric@hotmail.fr>
2025-05-21 17:35:18 +0200

    1.37.16

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -11,10 +11,7 @@ export default function Index() {
       }}
     >
       <Text className="font-bold text-lg my-10 font-rubik text-3xl">Welcome to ReState</Text>
-      <Link href="/sign-in">SignIn</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href={{ pathname: "/properties/[id]", params: { id: "1" } }}>Property</Link>
+
     </View>
   );
 }
